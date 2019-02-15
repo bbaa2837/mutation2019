660c1989d1f5a8eea983e88d759b3c5bfa3b99f9
bbaa2837@kasit.ac.kr <bbaa2837@kaist.ac.kr>
2019-02-15 13:01:56 +0900

    edit

--- milliCheck.py
+++ milliCheck.py
@@ -10,10 +10,7 @@ import time
 def getIndex(str) :
     return re.findall('\d+', str)[0]
 
-
 if __name__ == '__main__': 
-
-    t = time.time()
 
     SPACE_DIR = "../"
 
@@ -21,34 +18,32 @@ if __name__ == '__main__':
     result_file = csv.writer(open("time.csv", "w", newline = ''))
     suitelist = list(reader)[0]
 
-    # for i, suite_index in enumerate(suitelist[1:]) :
+    for i, suite_index in enumerate(suitelist[1:]) :
 
-    #     suite = "suite" + suite_index
-    #     o_suite_writer = open(suite+"_o.sh", "w")
-    #     p_suite_writer = open(suite+"_p.sh", "w")
+        suite = "suite" + suite_index
 
+        for i in range(30) :
+            exec_time = [0,0]
 
-    exec_time = [0,0]
-    cmd = "millisecond-time sh suite55_bin_o.sh"
-    exec_time[0] = time.time()
-    process = subprocess.Popen(cmd.split(), stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-    out, err = process.communicate()
-    print(out)
-    
-    # output = out.decode('ISO-8859-1').split('\n')
-    # print(out.decode('ISO-8859-1'))
-    # elapsed_time_ms = int(re.findall('\d+', output[-2])[-1])
-    # print elapsed_time_ms
+            cmd = "sh" + suite + "_bin_o.sh"
+            exec_time[0] = time.time()
+            process = subprocess.Popen(cmd.split(), stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+            out, err = process.communicate()
+            print(out)
+            
+            # output = out.decode('ISO-8859-1').split('\n')
+            # print(out.decode('ISO-8859-1'))
+            # elapsed_time_ms = int(re.findall('\d+', output[-2])[-1])
+            # print elapsed_time_ms
 
-    exec_time[0] = time.time() - exec_time[0]
+            exec_time[0] = time.time() - exec_time[0]
 
-    cmd = "millisecond-time sh suite55_bin_p.sh"
-    exec_time[1] = time.time()
-    process = subprocess.Popen(cmd.split(), stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-    out, err, = process.communicate()
-    print(out)
+            cmd = "sh" + suite + "_bin_p.sh"
+            exec_time[1] = time.time()
+            process = subprocess.Popen(cmd.split(), stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+            out, err, = process.communicate()
+            print(out)
 
-    exec_time[1] = time.time() - exec_time[1]
+            exec_time[1] = time.time() - exec_time[1]
 
-    print(exec_time)
-
+            result_file.writerow([suite,exec_time])
